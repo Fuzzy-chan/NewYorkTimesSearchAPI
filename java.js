b4877234043f85106e5dadd992cfd28645b19b3c
Fuzzychan <fuzzych4an@gmail.com>
2018-03-06 22:26:53 -0500

    working ajax with a for loop based on numberRecords

--- java.js
+++ java.js
@@ -1,19 +1,32 @@
 $(document).on("click", ".submit-btn", function(){
-    event.preventDefault();
-    var numberRecords = $("#numberRecords").val();
-    var searchTerm = $("#searchTerm").val().trim();
-    var startYear = $("#startYear").val().trim();
-    var endYear = $("#endYear").val().trim();
-    console.log(numberRecords, searchTerm, startYear, endYear)
+  event.preventDefault();
+  var numberRecords = $("#numberRecords").val();
+  var searchTerm = $("#searchTerm").val().trim();
+  var startYear = $("#startYear").val().trim();
+  var endYear = $("#endYear").val().trim();
+  console.log(numberRecords, searchTerm, startYear, endYear)    
 
-    var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?api_key=2664ad2903ec42308e32d21b90486032&q="+searchTerm+"&begin_date="+startYear+"0101&end_date="+endYear+"0101";
+  var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?api_key=2664ad2903ec42308e32d21b90486032&q="+searchTerm;
+  
+  if (startYear){
+    queryURL += "&begin_date="+startYear+"0101"
+  }
 
+  if (endYear){
+    queryURL += "&end_date="+endYear+"0101"
+  }
 
-    $.ajax({
-      url: queryURL,
-      method: 'GET',
-    }).then(function(result) {
-      console.log(result);    
-    });
+  $.ajax({
+    url: queryURL,
+    method: 'GET',
+  }).then(function(result) {
+    for (var i = 0; i<=numberRecords; i++){}
+    var webUrl = result.response.docs[i].web_url;
+    console.log(webUrl);
+    console.log(result);
+  
+    // selector(result.response.docs);
+//   });
 
-  });
+});
+});
