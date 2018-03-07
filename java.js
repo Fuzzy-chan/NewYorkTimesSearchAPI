be5c62af5ae1ee0991ed43eabb7b35078961be0e
Fuzzychan <fuzzych4an@gmail.com>
2018-03-06 21:03:21 -0500

    working AJAX, nothing is optional yet

--- java.js
+++ java.js
@@ -1,21 +1,19 @@
 $(document).on("click", ".submit-btn", function(){
     event.preventDefault();
     var numberRecords = $("#numberRecords").val();
-    var searchTerm = $("#searchTerm").val();
-    var startYear = $("#startYear").val();
-    var endYear = $("#endYear").val();
+    var searchTerm = $("#searchTerm").val().trim();
+    var startYear = $("#startYear").val().trim();
+    var endYear = $("#endYear").val().trim();
     console.log(numberRecords, searchTerm, startYear, endYear)
-    var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
-    url += '?' + $.param({
-      'api-key': "2664ad2903ec42308e32d21b90486032",
-      'q': searchTerm,
-      'begin_date': startYear,
-      'end_date': endYear,
-    });
+
+    var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?api_key=2664ad2903ec42308e32d21b90486032&q="+searchTerm+"&begin_date="+startYear+"0101&end_date="+endYear+"0101";
+
+
     $.ajax({
-      url: url,
+      url: queryURL,
       method: 'GET',
     }).then(function(result) {
       console.log(result);    
     });
-});
+
+  });
